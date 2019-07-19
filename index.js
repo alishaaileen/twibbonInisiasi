c77880b529e777136b71e7b749fa89d739ae6974
Vriyas Hartama <vriyashartama@gmail.com>
2019-07-19 23:40:53 +0700

    What is this.

--- index.js
+++ index.js
@@ -1,33 +1,40 @@
-function previewFile() {
+function el(id) {
+  return document.getElementById(id);
+} // Get elem by ID
 
-    // ---- Inputing uploaded file to src
-    var pic = document.getElementById('img2');
-    var file    = document.querySelector('input[type=file]').files[0];
-    var reader  = new FileReader();
-
-    reader.onloadend = function () {
-        pic.src = reader.result;
-    }
-
-    if (file) {
-        reader.readAsDataURL(file);
-    } else {
-        pic.src = "";
-    }
-    // --------------------------------
-    
-    var twibbon = new Image();
-    var picture = new Image();
-    var twibbon = document.getElementById('img1');
-    var picture = document.getElementById('img2');
-    var canvas = document.getElementById('canvas');
-    var context = canvas.getContext('2d');
-
-    canvas.width = twibbon.width;
-    canvas.height = twibbon.height;
-    context.drawImage(picture, 0, 0, canvas.width, canvas.height);
-    context.drawImage(twibbon, 0, 0);
-
-    var hasil = canvas.toDataURL("image/png");
-    document.getElementById('hasil').src = hasil;
+function wait(ms) {
+  var start = new Date().getTime();
+  var end = start;
+  while (end < start + ms) {
+    end = new Date().getTime();
+  }
 }
+
+var canvas = el("canvas");
+var context = canvas.getContext("2d");
+
+function readImage() {
+  if (this.files && this.files[0]) {
+    var FR = new FileReader();
+    FR.onload = function(e) {
+      var img = new Image();
+      img.addEventListener("load", function() {
+        context.drawImage(img, 0, 0, 1080, 1080);
+      });
+      img.src = e.target.result;
+      var twibbon = new Image();
+      twibbon.addEventListener("load", function() {
+        context.drawImage(twibbon, 0, 0, 1080, 1080);
+      });
+      twibbon.src = "http://127.0.0.1:5500/twibbon.png";
+    };
+    FR.readAsDataURL(this.files[0]);
+
+    var imageData = context.getImageData(0, 0, 1080, 1080);
+
+    var Pic = canvas.toDataURL("image/png");
+    console.log(Pic);
+  }
+}
+
+el("fileUpload").addEventListener("change", readImage, false);
